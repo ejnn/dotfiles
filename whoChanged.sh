77090f933ba7ca1560d3f6a6c2922422c6374882
Eduardo Jorge Nicolau <eduardojorgenicolau@gmail.com>
2020-11-17 11:14:40 -0300

    forgot .Xresources

--- whoChanged.sh
+++ whoChanged.sh
@@ -4,3 +4,4 @@ diff -q .config/i3/config ~/.config/i3/config
 diff -q .config/rofi/config.rasi ~/.config/rofi/config.rasi
 diff -q .emacs.d/init.el ~/.emacs.d/init.el
 diff -q .emacs.d/custom.el ~/.emacs.d/custom.el
+diff -q .Xresources ~/.Xresources
