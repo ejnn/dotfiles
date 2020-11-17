cb6794a3eb0111f9b09da66ef75fa452b8c68155
Eduardo Jorge Nicolau <eduardojorgenicolau@gmail.com>
2020-11-17 11:13:19 -0300

    updated some and added a diff script

---
+++ whoChanged.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+diff -q .config/i3/config ~/.config/i3/config
+diff -q .config/rofi/config.rasi ~/.config/rofi/config.rasi
+diff -q .emacs.d/init.el ~/.emacs.d/init.el
+diff -q .emacs.d/custom.el ~/.emacs.d/custom.el
